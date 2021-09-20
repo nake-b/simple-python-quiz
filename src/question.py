940fb09606498c1d8a9f9550940c1795d23a3e89
Nake <basicnadir@gmail.com>
2021-09-20 13:18:05 +0200

    added a module for handling questions

---
+++ src/question.py
@@ -0,0 +1,29 @@
+from data import get_token, get_data
+import html
+
+
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+    def __str__(self):
+        return f"{self.text} - {self.answer}"
+
+
+def get_questions(amount=10) -> list:
+    question_bank = []
+    question_token = get_token()
+    question_data = get_data(amount=amount,
+                             token=question_token,
+                             type="boolean")
+
+    for question in question_data:
+        text = question["question"]
+        text = html.unescape(text)
+        answer = question["correct_answer"]
+        new_question = Question(text, answer)
+        question_bank.append(new_question)
+
+    return question_bank
+
